3f14d0c581fa3cec5c6eb84374db64d7501fc6ee
MabinGo <bin.ma@huawei.com>
2017-08-16 17:37:01 +0800

    adjust project
    Signed-off-by: MabinGo <bin.ma@huawei.com>

--- seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/web/SeckillQueryRESTEndpoint.java
+++ seckill-query-service/src/main/java/io/servicecomb/poc/demo/seckill/web/SeckillQueryRESTEndpoint.java
@@ -2,14 +2,8 @@ package io.servicecomb.poc.demo.seckill.web;
 
 import io.servicecomb.poc.demo.seckill.SecKillEventSubscriber;
 import io.servicecomb.provider.rest.common.RestSchema;
-import io.swagger.annotations.Api;
 import java.util.List;
 import java.util.logging.Logger;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
